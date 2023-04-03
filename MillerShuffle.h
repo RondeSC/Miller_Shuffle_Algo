c427fc4495f64625f6cd3b3baaa0b6730b9ec653
Ron Miller <42258704+RondeSC@users.noreply.github.com>
2023-04-03 14:37:00 -0700

    commented out MillerShuffleAlgo_a() & MillerShuffleAlgo_b()

--- MillerShuffle.h
+++ MillerShuffle.h
@@ -1,7 +1,8 @@
 
 // Miller Shuffle Algorithm proto types
-unsigned int MillerShuffleAlgo_a(unsigned int, unsigned int, unsigned int);
-unsigned int MillerShuffleAlgo_b(unsigned int, unsigned int, unsigned int);
+//unsigned int MillerShuffleAlgo_a(unsigned int, unsigned int, unsigned int);
+//unsigned int MillerShuffleAlgo_b(unsigned int, unsigned int, unsigned int);
 unsigned int MillerShuffle      (unsigned int, unsigned int, unsigned int);
 unsigned int MillerShuffle_lite (unsigned int, unsigned int, unsigned int);
+unsigned int MillerShuffle_Max (unsigned int, unsigned int, unsigned int);
 unsigned int DDeck_Shuffle      (unsigned int, unsigned int, unsigned int);
