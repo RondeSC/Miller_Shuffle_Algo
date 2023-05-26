c181f3cd228f2b772602b0c35dfe8b4d5e70abb3
Ron Miller <42258704+RondeSC@users.noreply.github.com>
2023-05-25 22:43:39 -0700

    Update MillerShuffle.h

--- MillerShuffle.h
+++ MillerShuffle.h
@@ -3,6 +3,7 @@
 //unsigned int MillerShuffleAlgo_a(unsigned int, unsigned int, unsigned int);
 //unsigned int MillerShuffleAlgo_b(unsigned int, unsigned int, unsigned int);
 unsigned int MillerShuffle      (unsigned int, unsigned int, unsigned int);
+unsigned int MillerShuffleAlgo_e(unsigned int, unsigned int, unsigned int);
 unsigned int MillerShuffle_lite (unsigned int, unsigned int, unsigned int);
-unsigned int MillerShuffle_Max (unsigned int, unsigned int, unsigned int);
+unsigned int MillerShuffle_Max  (unsigned int, unsigned int, unsigned int);
 unsigned int DDeck_Shuffle      (unsigned int, unsigned int, unsigned int);
