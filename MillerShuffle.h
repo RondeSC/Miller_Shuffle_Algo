19d9a63d1f662bb5575a57370c08f64553f6dbc2
Ron Miller <42258704+RondeSC@users.noreply.github.com>
2023-12-13 17:00:37 -0800

    use #define MillerShuffle to set default Algorithm

--- MillerShuffle.h
+++ MillerShuffle.h
@@ -2,8 +2,10 @@
 // Miller Shuffle Algorithm proto types
 //unsigned int MillerShuffleAlgo_a(unsigned int, unsigned int, unsigned int);
 //unsigned int MillerShuffleAlgo_b(unsigned int, unsigned int, unsigned int);
-unsigned int MillerShuffle      (unsigned int, unsigned int, unsigned int);
+unsigned int MillerShuffleAlgo_d(unsigned int, unsigned int, unsigned int);
 unsigned int MillerShuffleAlgo_e(unsigned int, unsigned int, unsigned int);
 unsigned int MillerShuffle_lite (unsigned int, unsigned int, unsigned int);
-unsigned int MillerShuffle_Max  (unsigned int, unsigned int, unsigned int);
+//unsigned int MillerShuffle_Max  (unsigned int, unsigned int, unsigned int);
 unsigned int DDeck_Shuffle      (unsigned int, unsigned int, unsigned int);
+
+#define MillerShuffle MillerShuffleAlgo_d
