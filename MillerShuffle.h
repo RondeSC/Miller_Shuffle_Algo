250638243f6607428952289f36509cef84663e9e
Ron Miller <42258704+RondeSC@users.noreply.github.com>
2024-02-04 11:45:37 -0800

    made MSA_e default

--- MillerShuffle.h
+++ MillerShuffle.h
@@ -1,13 +1,13 @@
-
 // Miller Shuffle Algorithm proto types
 //unsigned int MillerShuffleAlgo_a(unsigned int, unsigned int, unsigned int);
 //unsigned int MillerShuffleAlgo_b(unsigned int, unsigned int, unsigned int);
 unsigned int MillerShuffleAlgo_d(unsigned int, unsigned int, unsigned int);
 unsigned int MillerShuffleAlgo_e(unsigned int, unsigned int, unsigned int);
-unsigned int MillerShuffle_lite (unsigned int, unsigned int, unsigned int);
-//unsigned int MillerShuffle_Max  (unsigned int, unsigned int, unsigned int);
+short int MillerShuffle_lite (short, unsigned long, short);
+short int MillerShuffle_xlite(short, unsigned long, short);
+
 unsigned int DDeck_Shuffle      (unsigned int, unsigned int, unsigned int);
 
-// perhaps better descriptive Names, based on application
-#define MillerShuffle MillerShuffleAlgo_d
-#define PRIG MillerShuffleAlgo_d
+// perhaps better descriptive Names, given the application
+#define MillerShuffle MillerShuffleAlgo_e
+#define PRIG MillerShuffleAlgo_e
